0e96a44dfa32fc46edbf61d7d7154a72db31f9f2
Arsenshmid <apce2@mail.ru>
2023-04-08 00:03:42 +0900

    ╨║╨╛╨╝╨╝╨╕╤В

--- PeopleCount/PeopleCount/Form2.cs
+++ PeopleCount/PeopleCount/Form2.cs
@@ -10,16 +10,18 @@ using System.Windows.Forms;
 
 namespace PeopleCount
 {
-    public partial class Form1 : Form
+    public partial class Form2 : Form
     {
-        public Form1()
+        public Form2()
         {
             InitializeComponent();
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
-            this.Close();
+            this.Hide(); // Скрываем Form2
+            Form1 form1 = new Form1();
+            form1.Show(); // Отображаем Form1
         }
     }
 }
