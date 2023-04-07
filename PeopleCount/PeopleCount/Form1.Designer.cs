ae0df4ae025a8a698cb6d2377ebd84fb7d6528e0
Arsenshmid <apce2@mail.ru>
2023-04-07 22:43:06 +0900

    ╨║╨╛╨╝╨╝╨╕╤В

--- PeopleCount/PeopleCount/Form1.Designer.cs
+++ PeopleCount/PeopleCount/Form1.Designer.cs
@@ -35,29 +35,32 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(127, 114);
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(136, 118);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(114, 42);
+            this.button1.Size = new System.Drawing.Size(176, 42);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Материалы Сайсарского округа";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(127, 162);
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Location = new System.Drawing.Point(136, 166);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(114, 42);
+            this.button2.Size = new System.Drawing.Size(176, 42);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "карта";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(127, 210);
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.Location = new System.Drawing.Point(136, 214);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(114, 42);
+            this.button3.Size = new System.Drawing.Size(176, 42);
             this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
+            this.button3.Text = "выход";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // Form1
@@ -65,11 +68,12 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(378, 409);
+            this.ClientSize = new System.Drawing.Size(454, 409);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "PeopleCount";
             this.ResumeLayout(false);
 
